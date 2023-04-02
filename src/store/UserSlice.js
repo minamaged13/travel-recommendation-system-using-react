e7f7feb98184d6c46cfa505364dd926acfc532f7
Salah Ashraf <salahashraf924@gmail.com>
2023-04-02 22:28:45 +0200

    update userSlice

--- src/store/UserSlice.js
+++ src/store/UserSlice.js
@@ -1,15 +1,25 @@
-import User from "@/models/User";
-import { createSlice } from "@reduxjs/toolkit";
-const initialState = { userID: 2, isLoggedin:false  };
-const UserSlice = createSlice({
-  name: "currentUser",
+import User from '@/models/User';
+import {createSlice} from '@reduxjs/toolkit';
+const initialState = {userID: 2, isLoggedin: false,firstName:"",secondName:"", email:""  };
+const UserSlice = createSlice ({
+  name: 'currentUser',
   initialState,
-  reducers: { logIn(state,action) {
-    state.userID= action.payload
+  reducers: {
+    logIn (state, action) {
+      state.isLoggedin = true;
+      state.userID = action.payload.id;
+      state.firstName = action.payload.firstName;
+      state.secondName = action.payload.secondName;
+      state.email = action.payload.email;
+    },
+
+    logout (state) {
+      state.userID = 0;
+    },
+    // loggedIn (state) {
+    //   state.isLoggedin = true;
+    // },
   },
-logout(state){
-    state.userID= 0;
-} },
 });
-export const UserActions= UserSlice.actions;
+export const UserActions = UserSlice.actions;
 export default UserSlice.reducer;
