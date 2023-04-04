984a70667d1a97d12b3f5d67265423e8c55d4521
Salah Ashraf <salahashraf924@gmail.com>
2023-04-04 02:50:42 +0200

    add image configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['encrypted-tbn0.gstatic.com', 'www.google.com'],
+  }
 }
-
 module.exports = nextConfig
