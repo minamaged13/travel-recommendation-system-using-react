164f31c3aafbda652ba7c4124485631338b37fed
Salma Magdy Ishak <salma.m.ishakk@gmail.com>
2023-04-06 22:41:01 +0200

    Update Home Page

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -5,10 +5,12 @@ const Card=(props)=>{
 const h= props.card.h
 const p= props.card.p
 const classname= props.classname
+const src= props.card.src
     return <Fragment>
         <div  className={classname}>
-    <h5 className="mb-2 font-mono text-4xl font-bold text-center  tracking-tight text-white dark:text-white">{h}</h5>
-    <p className="font-mono text-2xl text-white dark:text-white">{p}</p>
+    <h5 className="text-5xl sm:text-4xl text-center font-bold" >{h}</h5>
+    <img src={src} class=" mt-10  object-cover w-50 h-full rounded-lg"></img>
+    <p className=" text-2xl text-black mt-10 text-center" >{p}</p>
 </div>
 
     </Fragment>
